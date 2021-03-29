68b2c17fdb5c9fd9cb204718ae9a276a8de11079
JoonKi-Kim <joonki402@gmail.com>
2021-03-30 03:54:05 +0900

    요구사항 두번째 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,7 +3,9 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
@@ -33,6 +35,19 @@ public class RequestHandler extends Thread {
 //                log.debug("### : {}", line);
 //            }
             String url = HttpRequestUtils.getUrl(line);
+            if (url.startsWith("/create")) {
+                int index = url.indexOf("?");
+                String requestPath = url.substring(0, index);
+                String queryString = url.substring(index+1);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("User : {}", user);
+
+                url = "/index.html";
+            }
+            // http://localhost:8080/create?userId=test&password=test&name=test&email=test%40test.com
+
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
